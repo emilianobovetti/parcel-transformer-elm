8bec96d4b64f608f85aa34a92b420a3228974a03
Tommaso <p.tommy93@gmail.com>
2021-11-17 09:48:14 +0100

    feat: scrollable error page
    * Make error div scrollable
    
    * Bump to 0.0.6

--- error-page.js
+++ error-page.js
@@ -9,7 +9,7 @@ if (overlay) {
 overlay = document.createElement('div');
 overlay.id = OVERLAY_ID;
 
-overlay.innerHTML = `<div style="background: black; opacity: 0.85; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; font-family: Menlo, Consolas, monospace; z-index: 9999;">
+overlay.innerHTML = `<div style="background: black; opacity: 0.85; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; font-family: Menlo, Consolas, monospace; z-index: 9999; overflow: auto;">
   <div>
     <div style="font-size: 18px; font-weight: bold; margin-top: 20px;">
       🚨 Elm Compiler Error
